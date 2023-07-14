f1f28694ce0197a3a35960b71498e87c68afcfc0
Leo Izen <leo.izen@gmail.com>
2023-07-14 18:53:50 -0400

    add chunk stripper

--- umbrielpng.c
+++ umbrielpng.c
@@ -7,6 +7,9 @@
 
 #include <zlib.h>
 
+#define maketag(a,b,c,d) ((((uint32_t)(a)) << 24) | (((uint32_t)(b)) << 16) |\
+                         (((uint32_t)(c)) << 8) | (uint32_t)(d))
+
 typedef struct UmbPngChunk {
     uint32_t chunk_size;
     uint32_t tag;
@@ -16,14 +19,54 @@ typedef struct UmbPngChunk {
     size_t offset;
 } UmbPngChunk;
 
-typedef struct UmbPngFile {
+typedef struct UmbPngChunkChain {
     UmbPngChunk *chunk;
-    struct UmbPngFile *prev;
-    struct UmbPngFile *next;
-} UmbPngFile;
+    struct UmbPngChunkChain *prev;
+    struct UmbPngChunkChain *next;
+} UmbPngChunkChain;
+
+typedef struct UmbPngScanData {
+    int have_cicp;
+    int have_iccp;
+    int have_srgb;
+    int have_plte;
+} UmbPngScanData;
 
 static const uint8_t png_signature[8] = {
     0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a,
+};
+
+#define tag_IHDR maketag('I','H','D','R')
+#define tag_PLTE maketag('P','L','T','E')
+#define tag_IDAT maketag('I','D','A','T')
+#define tag_IEND maketag('I','E','N','D')
+
+#define tag_cICP maketag('c','I','C','P')
+#define tag_iCCP maketag('i','C','C','P')
+#define tag_sRGB maketag('s','R','G','B')
+#define tag_cHRM maketag('c','H','R','M')
+#define tag_gAMA maketag('g','A','M','A')
+
+#define tag_fdAT maketag('f','d','A','T')
+#define tag_acTL maketag('a','c','T','L')
+#define tag_fcTL maketag('f','c','T','L')
+
+#define tag_sBIT maketag('s','B','I','T')
+#define tag_tRNS maketag('t','R','N','S')
+#define tag_hIST maketag('h','I','S','T')
+
+#define tag_bKGD maketag('b','K','G','D')
+#define tag_eXIf maketag('e','X','I','f')
+#define tag_pHYs maketag('p','H','Y','s')
+#define tag_sPLT maketag('s','P','L','T')
+#define tag_tIME maketag('t','I','M','E')
+#define tag_tEXt maketag('t','E','X','t')
+#define tag_zTXt maketag('z','T','X','t')
+#define tag_iTXt maketag('i','T','X','t')
+
+static const uint32_t strip_chunks[8] = {
+    tag_bKGD, tag_eXIf, tag_pHYs, tag_sPLT,
+    tag_tIME, tag_tEXt, tag_zTXt, tag_iTXt,
 };
 
 static inline uint32_t tag_array_to_uint32(const uint8_t *tag) {
@@ -117,8 +160,10 @@ int read_chunk(FILE *in, UmbPngChunk *chunk, char **error) {
         total += read;
     }
 
-    if (crc != chunk->crc32)
+    if (crc != chunk->crc32) {
         fprintf(stderr, "Warning: computed CRC32 %08x does not match read CRC32 %08x\n", crc, chunk->crc32);
+        chunk->crc32 = crc;
+    }
 
     return 0;
 
@@ -129,9 +174,44 @@ fail:
     return -1;
 }
 
+int write_chunk(FILE *out, UmbPngChunk *chunk, char **error) {
+    uint8_t tag[4];
+    size_t count;
+    size_t total = 0;
+
+    uint32_to_tag_array(tag, chunk->data_size);
+    count = fwrite(tag, 4, 1, out);
+    if (!count)
+        goto fail;
+    
+    uint32_to_tag_array(tag, chunk->tag);
+    count = fwrite(tag, 4, 1, out);
+    if (!count)
+        goto fail;
+
+    while (total < chunk->data_size) {
+        count = fwrite(chunk->data + total, 1, chunk->data_size - total, out);
+        if (!count)
+            goto fail;
+        total += count;
+    }
+
+    uint32_to_tag_array(tag, chunk->crc32);
+    count = fwrite(tag, 4, 1, out);
+    if (!count)
+        goto fail;
+
+    return 0;
+
+fail:
+    perror(*error);
+    *error = "Error writing chunk data";
+    return -1;
+}
+
 /** frees everything except the base node */
-void free_chain(UmbPngFile *file) {
-    UmbPngFile *prev = file;
+void free_chain(UmbPngChunkChain *file) {
+    UmbPngChunkChain *prev = file;
     if (!file)
         return;
     do {
@@ -153,11 +233,14 @@ void free_chain(UmbPngFile *file) {
 int main(int argc, char *argv[]) {
     int ret = 0;
     const char *input = argc > 1 ? argv[1] : "-";
+    const char *output = argc > 2 ? argv[2] : NULL;
     FILE *in = NULL;
+    FILE *out = NULL;
     uint8_t sig[8];
-    size_t read;
-    UmbPngFile png_file = { 0 };
-    UmbPngFile *curr_chain = &png_file;
+    size_t count;
+    UmbPngChunkChain png_file = { 0 };
+    UmbPngChunkChain *curr_chain = &png_file;
+    UmbPngScanData data = { 0 };
     
     if (!strcmp("-", input))
         in = stdin;
@@ -169,9 +252,12 @@ int main(int argc, char *argv[]) {
         goto flush;
     }
 
-    read = fread(sig, 8, 1, in);
-    if (!read) {
-        perror(argv[0]);
+    count = fread(sig, 8, 1, in);
+    if (!count) {
+        if (ferror(in))
+            perror(argv[0]);
+        else if (feof(in))
+            fprintf(stderr, "%s: Premature end of file\n", argv[0]);
         ret = 2;
         goto flush;
     }
@@ -197,7 +283,7 @@ int main(int argc, char *argv[]) {
                 goto flush;
             }
         } else {
-            UmbPngFile *next = calloc(1, sizeof(UmbPngFile));
+            UmbPngChunkChain *next = calloc(1, sizeof(UmbPngChunkChain));
             if (!next) {
                 fprintf(stderr, "%s: Allocation failure\n", argv[0]);
                 ret = 2;
@@ -218,6 +304,20 @@ int main(int argc, char *argv[]) {
                 goto flush;
             }
         }
+        switch (curr_chain->chunk->tag) {
+        case tag_PLTE:
+            data.have_plte = 1;
+            break;
+        case tag_cICP:
+            data.have_cicp = 1;
+            break;
+        case tag_sRGB:
+            data.have_srgb = 1;
+            break;
+        case tag_iCCP:
+            data.have_iccp = 1;
+            break;
+        }
         uint32_to_tag_array(tag, curr_chain->chunk->tag);
         fprintf(stderr, "Chunk: %s, Size: %u, Offset: %llu, CRC32: %08x\n",
             tag, curr_chain->chunk->chunk_size, (long long unsigned)curr_chain->chunk->offset, curr_chain->chunk->crc32);
@@ -232,9 +332,66 @@ int main(int argc, char *argv[]) {
         curr_chain = curr_chain->next;
     }
 
+    if (!output)
+        goto flush;
+
+    fclose(in);
+    in = NULL;
+
+    if (!strcmp("-", output))
+        out = stdout;
+    else
+        out = fopen(output, "w");
+    if (!out) {
+        perror(argv[0]);
+        ret = 1;
+        goto flush;
+    }
+
+    count = fwrite(png_signature, 8, 1, out);
+    if (!count) {
+        perror(argv[0]);
+        ret = 2;
+        goto flush;
+    }
+
+    curr_chain = &png_file;
+    while (curr_chain) {
+        char *error = argv[0];
+        int skip = 0;
+        uint8_t tag[5] = { 0 };
+        for (int i = 0; i < sizeof(strip_chunks)/sizeof(strip_chunks[0]); i++) {
+            if (strip_chunks[i] == curr_chain->chunk->tag) {
+                skip = 1;
+                break;
+            }
+        }
+        if (curr_chain->chunk->tag == tag_hIST && !data.have_plte)
+            skip = 1;
+        if ((curr_chain->chunk->tag == tag_cHRM || curr_chain->chunk->tag == tag_gAMA)
+                && (data.have_cicp || data.have_iccp || data.have_srgb))
+            skip = 1;
+        if (curr_chain->chunk->tag == tag_sRGB && data.have_iccp)
+            skip = 1;
+        /* not strictly spec compliant but cICP and sRGB both present is very likely just sRGB */
+        if (curr_chain->chunk->tag == tag_cICP && !data.have_iccp && data.have_srgb)
+            skip = 1;
+        if (skip) {
+            uint32_to_tag_array(tag, curr_chain->chunk->tag);
+            fprintf(stderr, "Stripping chunk: %s\n", tag);
+        } else {
+            ret = write_chunk(out, curr_chain->chunk, &error);
+            if (ret < 0)
+                goto flush;
+        }       
+        curr_chain = curr_chain->next;
+    }
+
 flush:
     if (in)
         fclose(in);
+    if (out)
+        fclose(out);
     free_chain(&png_file);
     return ret;
 }
